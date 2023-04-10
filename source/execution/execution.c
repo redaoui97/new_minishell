bf69125c6424ef1ce012f9e6bc1858d1ed973a15
Nabil Reda <redanabil14@gmail.com>
2023-04-10 05:53:22 +0000

    fixed the mac compatibility issue, working on pipes

--- source/execution/execution.c
+++ source/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 18:30:27 by rnabil            #+#    #+#             */
-/*   Updated: 2023/04/10 05:02:59 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/04/10 05:52:04 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,16 +118,21 @@ void	execute_command(t_cmd *cmd)
 	pid = fork();
 	if (pid == -1)
 		return ((void)simple_error("failed to create a child process!"));
+	//change this; make_envp adds even those env vars that weren't exported; only use on export
 	envp = make_envp();
 	if (pid == 0)
 	{		
 		path = get_path(cmd->cmd_args[0], find_path_env());
-		if (cmd->infile != -1 && cmd->outfile != -1)
+		if (cmd->infile != -1)
+		{
+			dup2(cmd->infile, 0);
+			close (cmd->infile);
+		}
+		if (cmd->outfile != -1)
 		{
 			dup2(cmd->outfile, 1);
-			dup2(cmd->infile, 0);
 			close (cmd->outfile);
-			close (cmd->infile);
+			
 		}
 		execve(path, cmd->cmd_args, envp);
 	}
@@ -136,6 +141,16 @@ void	execute_command(t_cmd *cmd)
 static void	set_pipes(t_cmd *cmd, int **pipes)
 {
 	
+}
+
+static void	close_files(t_cmd *cmd)
+{
+	//add error protection for close 
+	if (cmd->infile != -1)
+		close (cmd->infile);
+	if (cmd->outfile != -1)
+		close (cmd->outfile);
+	//close pipes
 }
 
 void	execute(t_cmd *cmds, int pipes_count)
@@ -150,7 +165,7 @@ void	execute(t_cmd *cmds, int pipes_count)
 	{
 		set_pipes(&cmds[i], &pipes[i]);
 		execute_command(&cmds[i]);
-		//close_pipes();
+		close_files(&cmds[i]);
 		wait_child();
 		i++;
 	}
