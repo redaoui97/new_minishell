f6ca1a9774a03a05ecbcf8328dee821c8907c636
red krow <redanabil14@gmail.com>
2023-04-15 07:24:47 +0000

    fixed the env showing env vars bug

--- source/execution/builtins/env.c
+++ source/execution/builtins/env.c
@@ -6,11 +6,25 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 04:28:39 by rnabil            #+#    #+#             */
-/*   Updated: 2023/04/14 22:33:57 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/04/15 07:22:33 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static int	has_value(char *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int	exec_env(t_cmd *cmds)
 {
@@ -24,7 +38,7 @@ int	exec_env(t_cmd *cmds)
 	ptr = g_gen.env;
 	while (ptr)
 	{
-		if (ptr->exp == 0)
+		if (has_value((char *)ptr->content))
 			printf("%s\n", (char *)ptr->content);
 		ptr = ptr->next;
 	}
