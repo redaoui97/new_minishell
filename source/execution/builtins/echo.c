525b70e61cb3781ac7347b59854bbca5307a392c
red krow <redanabil14@gmail.com>
2023-04-15 07:13:20 +0000

    fixed the -n echo bug

--- source/execution/builtins/echo.c
+++ source/execution/builtins/echo.c
@@ -6,11 +6,29 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 04:28:20 by rnabil            #+#    #+#             */
-/*   Updated: 2023/04/14 08:42:54 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/04/15 07:11:09 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static int	has_param(t_cmd *cmd)
+{
+	int	i;
+
+	i = 1;
+	if (!(cmd->cmd_args)[1])
+		return (0);
+	if ((cmd->cmd_args)[1][0] != '-' || !(cmd->cmd_args)[1][1])
+		return (0);
+	while ((cmd->cmd_args)[1][i])
+	{
+		if ((cmd->cmd_args)[1][i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	exec_echo(t_cmd *cmd)
 {
@@ -19,7 +37,7 @@ int	exec_echo(t_cmd *cmd)
 
 	param = 0;
 	i = 1;
-	if ((cmd->cmd_args)[1] && !ft_strcmp((cmd->cmd_args)[1], "-n"))
+	if ((cmd->cmd_args)[1] && has_param(cmd))
 	{
 		i = 2;
 		param = 1;
